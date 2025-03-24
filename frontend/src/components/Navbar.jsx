624d8ac9220b03606448f58c6907bc01f8e949b9
yuvraj <singhyuvraj0717@gmail.com>
2025-03-25 01:14:46 +0530

    Add some details

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <h1 className="text-lg font-bold">TalkEase</h1>
             </Link>
           </div>
 
