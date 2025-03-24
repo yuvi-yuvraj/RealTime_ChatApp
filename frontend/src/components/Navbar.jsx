6332140492b6135a58099f26f729af8b73ed5458
yuvraj <singhyuvraj0717@gmail.com>
2025-03-25 01:55:40 +0530

    Edit logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
-import { LogOut, MessageSquare, Settings, User } from "lucide-react";
+import { LogOut, Settings, User } from "lucide-react";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
@@ -15,7 +15,7 @@ const Navbar = () => {
           <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <MessageSquare className="w-5 h-5 text-primary" />
+                <img src="/chat.png" alt="chat" className="w-5 h-5 text-primary"/>
               </div>
               <h1 className="text-lg font-bold">TalkEase</h1>
             </Link>
--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -1,4 +1,3 @@
-import { MessageSquare } from "lucide-react";
 
 const NoChatSelected = () => {
   return (
@@ -11,16 +10,12 @@ const NoChatSelected = () => {
               className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
              justify-center animate-bounce"
             >
-              <MessageSquare className="w-8 h-8 text-primary " />
+              <img src="/chat.png" alt="chat" className="w-8 h-8 text-primary "/>
             </div>
           </div>
         </div>
 
-        {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
-        <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
-        </p>
+        <h2 className="text-2xl font-bold">Welcome to TalkEase!</h2>
       </div>
     </div>
   );
